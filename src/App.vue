537052335a7f8ef9425ce6d73d8b2425710321d1
Milan Vuckovic <vuckovic@autview.com>
2022-09-05 18:22:12 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="container">
+  <app-header></app-header>
+  <div class="row">
+    <div class="col-md-12">
+      <router-view></router-view>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+export default {
+  components: {
+    'appHeader': Header
+  },
+  created() {
+    this.$store.dispatch('initStocks');
+  }
+}
+</script>
+
+<style>
+  body {
+    background-color: #366635;
+    padding: 20px;
+  }
+</style>
